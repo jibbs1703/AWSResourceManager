85fa1dace3b96f088ad12ed5242a5bb739a29b45
Abraham Ajibade <97408939+jibbs1703@users.noreply.github.com>
2025-02-16 08:06:39 -0500

    added more methods to S3 handler class

--- src/aws_resource_manager/s3/s3.py
+++ src/aws_resource_manager/s3/s3.py
@@ -1,3 +1,4 @@
+import io
 import os
 from io import StringIO
 
@@ -83,7 +84,7 @@ class S3Handler:
             logger.error(f"An AWS client error occurred: {e}")
             return None
 
-    def create_bucket(self, bucket_name) -> None:
+    def create_bucket(self, bucket_name: str) -> None:
         """
         Creates an S3 bucket in the user's AWS account.
 
@@ -110,8 +111,49 @@ class S3Handler:
 
         except Exception as e:  # noqa: BLE001
             logger.error(f"An error occurred while creating the bucket: {e}")
+    
+    def delete_bucket(self, bucket_name: str) -> None:
+        """
+        Deletes an empty bucket in the user's S3 account.
 
-    def list_objects(self, bucket_name):
+        Parameters:
+        bucket_name (str): The name of the bucket to be deleted.
+
+        Returns:
+        None
+
+        Raises:
+        ClientError: If the bucket is not empty, does not exist, or if there are 
+        insufficient permissions to delete the bucket.
+
+        """
+        try:
+            logger.info(f"Deleting S3 bucket {bucket_name}")
+            self.client.delete_bucket(Bucket=bucket_name)
+            logger.info(f"S3 bucket {bucket_name} deleted successfully.")
+        except ClientError as e:
+            logger.error(f"Error deleting S3 bucket {bucket_name}: {e}")
+
+    def check_object_exists(self, bucket_name:str, object_name:str):
+        """
+        Checks if an object exists in the S3 bucket.
+
+        :param object_name: The name of the object to check
+        :return: True if the object exists, False otherwise
+        """
+        try:
+            logger.info(f"Checking if object {object_name} exists in bucket {bucket_name}")
+            self.client.head_object(Bucket=bucket_name, Key=object_name)
+            logger.info(f"Object {object_name} exists in bucket {bucket_name}")
+            return True
+        except ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                logger.warning(f"Object {object_name} not found in bucket {bucket_name}")
+            else:
+                logger.error(f"Error checking if object {object_name} exists in S3: {e}")
+            return False
+
+    def list_objects(self, bucket_name: str) -> list:
         """
         Lists the objects in an S3 bucket.
 
@@ -138,14 +180,56 @@ class S3Handler:
             logger.exception(f"Unexpected error listing objects: {e}")
             return None
 
-    def upload_file(self, bucket_name, filename, file, folder=""):
+    def delete_object(self, bucket_name:str, object_name:str, folder="")-> None:
+        """
+        Deletes an object from the S3 bucket.
+
+        :param object_name: The name of the object to delete
+        """
+        try:
+            logger.info(f"Deleting object {object_name} from bucket {bucket_name}")
+            self.client.delete_object(Bucket=bucket_name, Key=f"{folder}{object_name}")
+            logger.info(f"Object {object_name} deleted from bucket {bucket_name}")
+        except ClientError as e:
+            self.logger.error(f"Error deleting object {object_name} from S3: {e}")
+
+    def generate_presigned_url(self, bucket_name:str, object_name:str, 
+                               expiration:int=600)->str:
+        """
+        Generates a pre-signed URL to access an S3 object in the specified bucket.
+
+        This method creates a pre-signed URL that allows temporary access to an S3 object
+        without requiring AWS credentials. The URL can be shared with others to grant 
+        access to the object for a limited time.
+
+        :param bucket_name: The name of the S3 bucket containing the object.
+        :param object_name: The key (name) of the S3 object to be accessed.
+        :param expiration: The expiration time in seconds for the pre-signed URL 
+                        (default is 600 seconds or 10 minutes).
+        :return: A pre-signed URL as a string that provides temporary access to the S3 object.
+        :raises: ClientError if there is an error generating the pre-signed URL.
+
+        """
+        try:
+            logger.info(f"Generating pre-signed URL for {object_name} in  bucket {bucket_name}")
+            url = self.client.generate_presigned_url('get_object',
+                                                 Params={'Bucket': bucket_name, 'Key': object_name},
+                                                 ExpiresIn=expiration)
+            logger.info(f"Generated pre-signed URL for object {object_name}")
+            return url
+        except ClientError as e:
+            logger.error(f"Error generating pre-signed URL for {object_name}: {e}")
+            return None
+
+    def upload_file(self, bucket_name:str, filename:str, file:io.BytesIO, folder:str=""):
         """
         Uploads a file to an S3 bucket.
         Parameters:
         - bucket_name (str): The name of the target S3 bucket.
         - filename (str): The name of the file to be uploaded.
         - file (io.BytesIO): The file object to be uploaded.
-        - folder (str, optional): The folder path within the S3 bucket. Default is an empty string.
+        - folder (str, optional): The folder path within the S3 bucket.
+          Default is an empty string.
 
         Returns: None
         """
@@ -162,7 +246,7 @@ class S3Handler:
         except Exception as e:  # noqa: BLE001
             logger.error(f"Error uploading file to S3: {str(e)}")
 
-    def download_file(self, bucket_name, object_name, file_name) -> None:
+    def download_file(self, bucket_name:str, object_name:str, file_name:str) -> None:
         """
         Downloads a file from an S3 bucket in the user's AWS account.
 
@@ -184,7 +268,7 @@ class S3Handler:
         except Exception as e:
             logger.exception(f"Unexpected error downloading file: {e}")
 
-    def read_file(self, bucket_name, object_name):
+    def read_file(self, bucket_name: str, object_name: str):
         """
         Reads a file from an S3 bucket in the user's AWS account and returns its contents.
 
@@ -217,7 +301,7 @@ class S3Handler:
             logger.exception(f"Unexpected error reading file: {e}")
             return None
 
-    def delete_file(self, bucket_name, file_name):
+    def delete_file(self, bucket_name:str, file_name:str):
         """
         Deletes a file from an S3 bucket in the user's AWS account.
 
